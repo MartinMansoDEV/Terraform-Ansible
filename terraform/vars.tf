e663411cc2924a66cd8db193e05601b336c7e482
Martín Manso <martinmanso.dev@gmail.com>
2021-06-21 21:29:32 +0200

    Probado el count para las redes

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,3 +4,16 @@ variable "vm_size" {
   description = "Tamaño de la máquina virtual"
   default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
+
+variable "envs" {
+  type = list(string)
+  description = "Entornos"
+  default = [ "dev", "pre" ]
+}
+
+
+variable "vms" {
+  type = list(string)
+  description = "Maquinas virtuales"
+  default = [ "nfs", "master", "worker1", "worker2"]
+}
