73c44652691fc5c980a4ebdc51a7e5386fb36bbb
Martín Manso <martinmanso.dev@gmail.com>
2021-07-04 01:05:18 +0200

    ♻REFACTOR: Cambio en la creacion de las maquinas

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,19 +1,12 @@
 
-variable "vm_size" {
-  type = string
-  description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
-}
 
-variable "envs" {
-  type = list(string)
-  description = "Entornos"
-  default = [ "dev", "pre" ]
-}
-
-
+#  ESPECIFICACIONES DE LAS MAQUINAS QUE SE VAN A CREAR
 variable "vms" {
-  type = list(string)
+  type = list(map(string))
   description = "Maquinas virtuales"
-  default = [ "nfs", "master", "worker1", "worker2"]
+  default = [ 
+    {"name": "master-nfs", "size": "Standard_D2_v3"}, # 8 GB, 2 CPU 
+    {"name": "worker1",    "size": "Standard_D1_v2"}, # 3.5 GB, 1 CPU 
+    {"name": "worker2",    "size": "Standard_D1_v2"}  # 3.5 GB, 1 CPU 
+  ]
 }
