27297883417f4ee5898c70f7c8549f7f5c443605
Martín Manso <martinmanso.dev@gmail.com>
2021-07-04 01:11:30 +0200

    ♻REFACTOR: Mejorada la creacion de recursos por bucles

--- terraform/network.tf
+++ terraform/network.tf
@@ -27,36 +27,40 @@ resource "azurerm_subnet" "mySubnet" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
 
 resource "azurerm_network_interface" "myNic1" {
-  name                = "vmnic1"  
+
+  count               = length(var.vms)
+  name                = "interface-${var.vms[count.index].name}"  
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-    ip_configuration {
-    name                           = "myipconfiguration1"
+  ip_configuration {
+    name                           = "myipconfiguration-${var.vms[count.index].name}"
     subnet_id                      = azurerm_subnet.mySubnet.id 
     private_ip_address_allocation  = "Static"
-    private_ip_address             = "10.0.1.10"
-    public_ip_address_id           = azurerm_public_ip.myPublicIp1.id
+    private_ip_address             = "10.0.1.${10 + count.index}"
+    public_ip_address_id           = element(azurerm_public_ip.myPublicIp.*.id, count.index)
   }
 
-    tags = {
-        environment = "CP2"
-    }
+  tags = {
+      environment = "CP2"
+  }
 
 }
 
 # IP pública
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
 
-resource "azurerm_public_ip" "myPublicIp1" {
-  name                = "vmip1"
+resource "azurerm_public_ip" "myPublicIp" {
+
+  count               = length(var.vms) 
+  name                = "vmip-${var.vms[count.index].name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 
-    tags = {
-        environment = "CP2"
-    }
+  tags = {
+      environment = "CP2"
+  }
 
 }
