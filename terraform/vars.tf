46d6ccb456d6f602c2462b81d160aba5fed0615c
Martín Manso <martinmanso.dev@gmail.com>
2021-07-17 13:38:13 +0200

    Añadido 4 servidor nfs para azure

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,6 +7,7 @@ variable "vms" {
   default = [ 
     {"name": "kubemaster", "ip": "10.0.1.10", "size": "Standard_D2_v3"}, # 8 GB,   2 CPU 
     {"name": "kubenode1",  "ip": "10.0.1.11", "size": "Standard_D1_v2"}, # 3.5 GB, 1 CPU 
-    {"name": "kubenode2",  "ip": "10.0.1.12", "size": "Standard_D1_v2"}  # 3.5 GB, 1 CPU 
+    {"name": "kubenode2",  "ip": "10.0.1.12", "size": "Standard_D1_v2"},  # 3.5 GB, 1 CPU 
+    {"name": "kubenfs",    "ip": "10.0.1.13", "size": "Standard_D1_v2"}  # 3.5 GB, 1 CPU 
   ]
 }
